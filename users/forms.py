2ddd85613f623000e1a555c9523817a164965873
april <martinezapril56@yahoo.com>
2018-11-07 22:35:26 -0500

    aprik

--- users/forms.py
+++ users/forms.py
@@ -19,7 +19,9 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
+
 class ProfileUpdateForm(forms.ModelForm):
+
     class Meta:
         model = Profile
         fields = ['image']      
--- users/views.py
+++ users/views.py
@@ -7,14 +7,15 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
+        if form.is_valid():             
             username = form.cleaned_data.get('username')
+            form.save()
             messages.success(
-            request, f'Your account has been created! You are now able to log in')
+                request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
+
     return render(request, 'users/register.html', {'form': form})
 
 
@@ -23,8 +24,8 @@ def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
-                                request.FILES,
-                                instance=request.user.profile)
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
